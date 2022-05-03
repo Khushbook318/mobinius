c7ed6d8d99de1a976379d538e906a678182fbfaa
Khushbook318 <kkumari@msystechnologies.com>
2022-05-03 12:39:28 +0530

    updated Banner page

--- Component/Banner.js
+++ Component/Banner.js
@@ -3,22 +3,22 @@ import React from 'react'
 function Banner() {
   return (
     <>
-       <div class="index-banner " howu-index="866">
-           <div class="home-landing-block pt-5" howu-index="865">
-               <div class="container" howu-index="864">
-                   <div class="row" howu-index="863">
-                       <div class="col-lg-7 order-md-1 text-center pb-5 px-5 d-none" howu-index="862">
+       <div className="index-banner " howu-index="866">
+           <div className="home-landing-block pt-5" howu-index="865">
+               <div className="container" howu-index="864">
+                   <div className="row" howu-index="863">
+                       <div className="col-lg-7 order-md-1 text-center pb-5 px-5 d-none" howu-index="862">
                            <img src="/assets/digital-and-beyond.svg" howu-index="861"/>
                         </div>
-                        <div class="col-lg-5 text-lg-left text-center py-lg-5 mt-lg-5 mb-4 mb-md-0" howu-index="860">
-                            <div class="banner-content-sec" howu-index="859">
-                                <h2 class="display-4 pt-5 mt-5" howu-index="858">
-                                    <span class="font-weight-bold pt-5 text-mdark" howu-index="857">Experience<br howu-index="856"/>
+                        <div className="col-lg-5 text-lg-left text-center py-lg-5 mt-lg-5 mb-4 mb-md-0" howu-index="860">
+                            <div className="banner-content-sec" howu-index="859">
+                                <h2 className="display-4 pt-5 mt-5" howu-index="858">
+                                    <span className="font-weight-bold pt-5 text-mdark" howu-index="857">Experience<br howu-index="856"/>
                                     Digital and<br howu-index="855"/>
                                     Beyond</span>
                                 </h2>
-                                <h6 class="muli my-4 text-mgrey" howu-index="854">Experts in AI &amp; ML, IoT, Cloud , DevOps &amp; specializing in App Development</h6>
-                                <p class="mt-5" howu-index="853"><a class="px-5 py-3 h5 muli text-decoration-none mobinius-button font-weight-bold text-white" href="/contact-us" howu-index="852"><span class="px-md-0 px-4 px-lg-5" howu-index="851"> Let’s Talk </span></a></p>
+                                <h6 className="muli my-4 text-mgrey" howu-index="854">Experts in AI &amp; ML, IoT, Cloud , DevOps &amp; specializing in App Development</h6>
+                                <p className="mt-5" howu-index="853"><a className="px-5 py-3 h5 muli text-decoration-none mobinius-button font-weight-bold text-white" href="/contact-us" howu-index="852"><span className="px-md-0 px-4 px-lg-5" howu-index="851"> Let’s Talk </span></a></p>
                             </div>
                         </div>
                     </div>
